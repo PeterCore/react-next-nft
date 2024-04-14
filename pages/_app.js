19f5de45da54c2d60d5da8f9165c0ee1a8060788
zhangchun <zhangchun@oamicnet.com>
2024-04-14 23:46:30 +0800

    fixbug: react nft next

---
+++ pages/_app.js
@@ -0,0 +1,14 @@
+import { NavBar } from '../components/componentsindex'
+import '../styles/globals.css'
+
+const MyApp = ({ Component, pageProps }) => (
+
+   <div>
+      <NavBar />
+      {/* <Component {...pageProps} /> */}
+
+   </div>
+)
+
+
+export default MyApp
